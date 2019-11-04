6705513df550053855004549a7d4b46d4ed40543
yahirrro <yahiro0921@gmail.com>
2019-11-04 19:20:35 +0900

    metaなど一部修正

--- components/aside.vue
+++ components/aside.vue
@@ -1,14 +1,35 @@
 <template>
   <aside class="side">
-    <section id="logo">
-      <img src="/icon/moripawiki.svg" alt />
-    </section>
-    <section id="login" class="d-flex">
-      <div class="link d-flex align-items-center">
-        <img src="https://minotar.net/helm/389b1a68f6474dd0a42161b6c22fdebe/100.png" alt />
-        <div class="text">Chocolatt</div>
-      </div>
-    </section>
+    <a href="https://wiki.morino.party">
+      <section id="logo">
+        <img src="/icon/moripawiki.svg" alt />
+      </section>
+    </a>
+
+    <a
+      :href="'/admin/#/collections/wiki/entries/'+this.$nuxt.$route.params.page"
+      style="text-decoration:none;"
+      v-if="this.$nuxt.$route.params.page"
+    >
+      <section id="login" class="d-flex justify-content-center">
+        <div class="link d-flex align-items-center">
+          <div class="icon">
+            <i class="fas fa-edit"></i>
+          </div>
+          <div class="text text-center">このページを編集する</div>
+        </div>
+      </section>
+    </a>
+    <a :href="'/admin/'" style="text-decoration:none;" v-if="!this.$nuxt.$route.params.page">
+      <section id="login" class="d-flex justify-content-center">
+        <div class="link d-flex align-items-center">
+          <div class="icon">
+            <i class="fas fa-edit"></i>
+          </div>
+          <div class="text text-center">投稿画面にアクセス</div>
+        </div>
+      </section>
+    </a>
 
     <nav id="side">
       <a href>
--- pages/_page.vue
+++ pages/_page.vue
@@ -5,8 +5,19 @@
       <section id="title">
         <div class="post-info">
           <h1>{{wikiPost.title}}</h1>
+          <p>{{wikiPost.description}}</p>
         </div>
         <div class="bg-color"></div>
+        <style v-if="wikiPost.image">
+  .all article.content section#title .bg {
+    z-index: 1;
+    height: 200px;
+    background: url({{wikiPost.image}}) 50% / cover no-repeat;
+  }
+  .all article.content section#title .bg:before {
+    display: none;
+  }
+        </style>
         <div class="bg"></div>
       </section>
       <section id="body">
@@ -22,12 +33,54 @@
 import side from "~/components/aside.vue";
 
 export default {
-  components: {
-    side
+  head() {
+    return {
+      title: this.wikiPost.title + " | もりのパーティ公式Wiki もりぱうぃき!",
+      script: [{ src: "https://kit.fontawesome.com/cf7cf76089.js" }],
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.wikiPost.description
+        },
+        {
+          property: "og:title",
+          content: this.wikiPost.title
+        },
+        {
+          property: "og:description",
+          content: this.wikiPost.description
+        },
+        {
+          property: "og:type",
+          content: "article"
+        },
+        {
+          property: "og:site_name",
+          content: "もりのパーティ!公式Wiki もりぱうぃき!"
+        },
+        {
+          property: "og:url",
+          content: "https://wiki.morino.party/" + this.$nuxt.$route.params.page
+        },
+        {
+          property: "og:image",
+          content: this.wikiPost.image
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image"
+        },
+        {
+          name: "twitter:site",
+          content: "morinoparty"
+        }
+      ]
+    };
   },
 
-  head: {
-    script: [{ src: "https://kit.fontawesome.com/cf7cf76089.js" }]
+  components: {
+    side
   },
   async asyncData({ params, payload }) {
     if (payload) return { wikiPost: payload };
