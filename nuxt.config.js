a405908328b80d9c054a9a21c80d69027ea4b311
yahirrro <yahiro0921@gmail.com>
2019-11-04 16:00:48 +0900

    動け

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,17 +51,5 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
-  },
-
-  generate: {
-    routes: function() {
-      const fs = require("fs");
-      return fs.readdirSync("./assets/content/wiki").map(file => {
-        return {
-          route: `/${file.slice(2, -5)}`, // Remove the .json from the end of the filename
-          payload: require(`./assets/content/wiki/${file}`)
-        };
-      });
-    }
   }
 };
